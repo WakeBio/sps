ce6e627e397d036b5a40c1033b74187cee50e382
Mark Woodward <mwoodward@cs.stanford.edu>
2025-07-25 11:24:37 -0700

    require exact argument matching in argparse

--- sps/server.py
+++ sps/server.py
@@ -2,7 +2,7 @@ import argparse
 from sps import Node
 
 def main():
-  parser = argparse.ArgumentParser(description="Launches an SPS Node in sever mode", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser = argparse.ArgumentParser(description="Launches an SPS Node in sever mode", allow_abbrev=False, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("--server_ips", nargs="+", default=[], help="list of known server ips on the network")
   parser.add_argument("--last_resort_server_ips", nargs="+", default=[], help="list of servers to use as a last resort")
   parser.add_argument("--network_port", type=int, default=8080, help="The port that the netork runs on")
